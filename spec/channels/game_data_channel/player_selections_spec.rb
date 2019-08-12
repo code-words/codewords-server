1935596c0c2a2f91ba2cf047c6bec7b722755f8f
Jon Peterson <github@joequincy.dev>
2019-08-12 14:22:22 -0600

    Add test that player cannot select full team

--- spec/channels/game_data_channel/player_selections_spec.rb
+++ spec/channels/game_data_channel/player_selections_spec.rb
@@ -54,7 +54,30 @@ describe GameDataChannel, type: :channel do
   xit 'rejects team/role selections once the game has started' do
   end
 
-  xit 'rejects a team selection if the team is full' do
+  it 'rejects a team selection if the team is full' do
+    archer = game.players.create(user: User.create(name: "Archer"), team: :red)
+    lana = game.players.create(user: User.create(name: "Lana"), team: :red)
+    player = game.players.create(user: User.create(name: "Cheryl"))
+    stub_connection current_player: player
+    subscription = subscribe
+
+    expect{subscription.select_team({"team" => "red"})}
+      .to have_broadcasted_to(game)
+      .from_channel(GameDataChannel)
+      .once
+      .with{ |data|
+        message = JSON.parse(data[:message], symbolize_names: true)
+        expect(message[:type]).to eq("illegal-action")
+
+        payload = message[:data]
+        expect(payload[:error]).to eq("The red team is full.")
+        expect(payload[:category]).to eq("personal")
+        expect(payload[:byPlayerId]).to eq(player.id)
+      }
+
+    player.reload
+    expect(player.team).to eq(nil)
+    expect(player.role).to eq(nil)
   end
 
   xit 'rejects a role selection if there are no more slots available for that role' do

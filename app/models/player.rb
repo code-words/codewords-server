b342ff3adc2f6bf1c1e26be91ddd04d82fcdbd6c
Jon Peterson <github@joequincy.dev>
2019-08-12 13:55:04 -0600

    Add can_join_team method to Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -12,4 +12,17 @@ class Player < ApplicationRecord
   def name
     user.name
   end
+
+  def can_join_team?(team)
+    players = game.players
+    players_on_team = players.count{|p| p.team == team}
+    if players_on_team < game.player_count / 2
+      if role && players.count{|p| p.team == team && p.role == role} == 0
+        return false
+      end
+      return true
+    else
+      return false
+    end
+  end
 end

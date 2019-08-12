5ed47eb5264790e4d87e812b361fec37d87516d4
Jon Peterson <github@joequincy.dev>
2019-08-12 14:22:30 -0600

    Add test that player cannot select full role

--- spec/channels/game_data_channel/player_selections_spec.rb
+++ spec/channels/game_data_channel/player_selections_spec.rb
@@ -80,7 +80,30 @@ describe GameDataChannel, type: :channel do
     expect(player.role).to eq(nil)
   end
 
-  xit 'rejects a role selection if there are no more slots available for that role' do
+  it 'rejects a role selection if there are no more slots available for that role' do
+    archer = game.players.create(user: User.create(name: "Archer"), role: :intel)
+    lana = game.players.create(user: User.create(name: "Lana"), role: :intel)
+    player = game.players.create(user: User.create(name: "Cheryl"))
+    stub_connection current_player: player
+    subscription = subscribe
+
+    expect{subscription.select_team({"role" => "intel"})}
+      .to have_broadcasted_to(game)
+      .from_channel(GameDataChannel)
+      .once
+      .with{ |data|
+        message = JSON.parse(data[:message], symbolize_names: true)
+        expect(message[:type]).to eq("illegal-action")
+
+        payload = message[:data]
+        expect(payload[:error]).to eq("There are already two Intel players.")
+        expect(payload[:category]).to eq("personal")
+        expect(payload[:byPlayerId]).to eq(player.id)
+      }
+
+    player.reload
+    expect(player.team).to eq(nil)
+    expect(player.role).to eq(nil)
   end
 
   xit 'rejects a team selection if that team already has the sending player\'s role' do

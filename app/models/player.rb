4bdd261285d5bcb2e7e3ca26ada5385d95cbdf7d
Jon Peterson <github@joequincy.dev>
2019-08-12 14:13:44 -0600

    Add can_join_role? method to Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -17,13 +17,31 @@ class Player < ApplicationRecord
     players = game.players
     players_on_team = players.count{|p| p.team == team}
     if players_on_team < game.player_count / 2
-      if role && players.count{|p| p.team == team && p.role == role} == 0
+      if role && players.count{|p| p.team == team && p.role == role} > 0
         return false
       end
-      return true
     else
       return false
     end
+    return true
+  end
+
+  def can_join_role?(role)
+    players = game.players
+    players_with_role = players.count{|p| p.role == role}
+    if role == "intel" && players_with_role < 2
+      if team && players.count{|p| p.team == team && p.role == role} > 0
+        return false
+      end
+    elsif role == "spy" && players_with_role < game.player_count - 2
+      spies_per_team = (game.player_count - 2) / 2
+      if team && players.count{|p| p.team == team && p.role == role} >= spies_per_team
+        return false
+      end
+    else
+      return false
+    end
+    return true
   end
 
   def is_blue_team?
